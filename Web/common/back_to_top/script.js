a11c9fbe9a850bb8251370ca253b2b34eb0d03c5
eclipsehx <1240204622@qq.com>
2016-12-04 21:45:28 +0800

    Add files via upload

---
+++ Web/common/back_to_top/script.js
@@ -0,0 +1,51 @@
+window.onload = function() {
+
+	var obtn = document.getElementById('btn');
+
+	//获取页面可视区的高度
+	var clientHeight = document.documentElement.clientHeight;
+
+	var timer = null;
+	var isTop = true;
+
+	window.onscroll = function() {
+
+		//获取滚动条距离顶部的高度
+		var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+		if (osTop >= clientHeight) {
+			//显示按钮
+			obtn.style.display = "block";
+		} else {
+			//隐藏按钮
+			obtn.style.display = "none";
+		}
+
+		if (!isTop) {
+			clearInterval(timer);
+		}
+
+		isTop = false;
+	}
+
+	obtn.onclick = function() {
+
+		//设置定时器
+		timer = setInterval(function() {
+
+			var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+			var ispeed = Math.floor( - osTop / 6);
+
+			document.documentElement.scrollTop = document.body.scrollTop = osTop + ispeed;
+
+			isTop = true;
+
+			console.log(osTop - ispeed);
+
+			if (osTop == 0) {
+				clearInterval(timer);
+			}
+		},
+		30);
+	}
+}
